0aa8020bdc7a84884bfc347d3f7a267c07cb4b25
andrey-aka-skif <2119163@gmail.com>
2021-11-14 02:43:17 +0300

    Добавьте файлы проекта.

---
+++ WPF/Explorer.WPF.UI/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Explorer.WPF.UI
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow mainVindow = new MainWindow();
+
+            mainVindow.Show();
+        }
+    }
+}
